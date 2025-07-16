5b028099eb7db93b6b44fb56e7ed347332a4472b
wiliromario <wiliromarioakukom@gmail.com>
2025-07-16 18:35:28 +0700

    enh: [fe] add custom breakpoint 3xl

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,12 +10,10 @@ export default function HomePage() {
     <div
       className={`flex flex-col items-center justify-center min-h-screen p-7 bg-[${backgroundColor}]`}
     >
-      <div className="w-full sm:w-sm 2xl:w-md bg-white p-7 flex flex-col items-center justify-center gap-4 rounded-xl drop-shadow-xl">
-        <text className={textParagraph}>Kamu belum punya portofolio</text>
+      <div className="w-full sm:w-sm 3xl:w-md bg-white p-7 flex flex-col items-center justify-center gap-4 rounded-xl drop-shadow-xl">
+        <p className={textParagraph}>Kamu belum punya portofolio</p>
         <Link href={"/edit"}>
-          <Button>
-            <text className={textParagraph}>Buat Portofolio</text>
-          </Button>
+          <Button text="Buat Portofolio" />
         </Link>
       </div>
     </div>
