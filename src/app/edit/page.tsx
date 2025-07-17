f85bac497585dfd0875567fd84afc549d313eb45
wiliromario <wiliromarioakukom@gmail.com>
2025-07-17 17:47:01 +0700

    style: [fe] change background color

--- src/app/edit/page.tsx
+++ src/app/edit/page.tsx
@@ -1,14 +1,11 @@
 "use client";
 
 import Section from "src/components/Section";
-import { backgroundColor } from "src/constants";
 import Editor from "./Editor";
 
 export default function EditPage() {
   return (
-    <div
-      className={`flex flex-col min-h-screen bg-[${backgroundColor}] md:flex-row`}
-    >
+    <div className={`flex flex-col min-h-screen md:flex-row`}>
       <Editor />
       <div className="hidden md:block md:w-px md:my-5 md:bg-gray-300" />
       <Section title="Preview">Test</Section>
